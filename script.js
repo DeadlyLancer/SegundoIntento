a1c23d8ef4491461b932e471b5d269492eaf9da5
DeadlyLancer <Urielof_magma@hotmail.com>
2024-08-25 19:57:18 -0600

    El encriptador

---
+++ script.js
@@ -0,0 +1,49 @@
+const textarea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+const laLlave = {
+    "e": "enter",
+    "i": "imes",
+    "a": "ai",
+    "o": "ober",
+    "u": "ufat"
+                };
+
+const laLlaveInv = Object.fromEntries(
+    Object.entries(laLlave).map(([Swrd, Shld]) => [Shld, Swrd]));
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textarea.value);
+    mensaje.value = textoEncriptado;
+    textarea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textarea.value);
+    mensaje.value = textoDesencriptado;
+    textarea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(texto) {
+    let resultado = texto.toLowerCase();
+    for (const [Swrd, Shld] of Object.entries(laLlave)) {
+        resultado = resultado.replaceAll(Swrd, Shld);
+    }
+    return resultado;
+}
+
+function desencriptar(texto) {
+    let resultado = texto.toLowerCase();
+    for (const [Swrd, Shld] of Object.entries(laLlaveInv)) {
+        resultado = resultado.replaceAll(Swrd, Shld);
+    }
+    return resultado;
+}
+
+function copiar(){
+    const mensajeTextarea = document.querySelector(".mensaje");
+    navigator.clipboard.writeText(mensajeTextarea.value);
+}
+
